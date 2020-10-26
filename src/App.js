9fb38226be3aecd243da51c755b936a42a0a6ebd
João Mago <joao.mago@drlavatudo.com>
2020-10-25 22:39:09 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,125 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
+import genio from './genio1.jpg'
+import genioPergunta from './genio2.jpg'
+import genioErro from './genio3.jpg'
+import genioSucesso from './genio4.jpeg'
+import './App.css'
+
+const SCREENS = {
+  start: 'start',
+  game: 'game',
+  message: 'message',
+}
+
+const MESSAGES = {
+  start:
+    'Estou pensando em um número entre 1 e 300, dúvido que você o acerte em 9 tentativas!',
+  firstQuestion: 'Vamos lá! Em qual número estou pensando? (entre 1 e 300)',
+  wrongAnswer:
+    'Você errou! Mas ainda tem {triesNumber} tentativas! Em qual número estou pensando? (entre 1 e 300)',
+  success: 'Você acertou! Muito bem! O número era {correctNumber}.',
+  bad: 'Você errou! O número era {correctNumber}...',
+}
+
+const MAXTRIES = 8
+
+const App = () => {
+  const [screen, setScreen] = useState('start')
+  const [currentMessage, setCurrentMessage] = useState(MESSAGES.start)
+  const [extraInfo, setExtraInfo] = useState("")
+  const [tries, setTries] = useState(MAXTRIES)
+  const [number, setNumber] = useState(0)
+  const [inputValue, setInputValue] = useState('')
+
+  const startGame = () => {
+    setScreen(SCREENS.game)
+    setTries(MAXTRIES)
+    setNumber(Math.round(Math.random() * 300))
+    setCurrentMessage(MESSAGES.firstQuestion)
+    setExtraInfo("")
+  }
+
+  const tentativa = () => {
+    if (number === parseInt(inputValue)) {
+      setCurrentMessage(MESSAGES.success.replace('{correctNumber}', number))
+      setScreen(SCREENS.message)
+    } else {
+      setTries(tries - 1)
+      if (tries > 0) {
+        if(number > inputValue) {
+          setExtraInfo(`O meu número é maior que ${inputValue}`)
+        } else {
+          setExtraInfo(`O meu número é menor que ${inputValue}`)
+        }
+        setCurrentMessage(MESSAGES.wrongAnswer.replace("{triesNumber}", tries))
+      } else {
+        setCurrentMessage(MESSAGES.bad.replace('{correctNumber}', number))
+        setScreen(SCREENS.message)
+      }
+    }
+  }
+
+  const goBack = () => {
+    setScreen(SCREENS.start)
+    setCurrentMessage(MESSAGES.start)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {screen === SCREENS.start && (
+          <>
+            <img src={genio} className="App-logo" alt="logo" />
+            <p>{currentMessage}</p>
+            <a
+              className="App-link"
+              target="_blank"
+              rel="noopener noreferrer"
+              onClick={startGame}
+            >
+              Começar
+            </a>
+          </>
+        )}
+        {screen === SCREENS.game && (
+          <>
+            <img src={genioPergunta} className="App-logo" alt="logo" />
+            <p>{currentMessage}</p>
+            <p>{extraInfo}</p>
+            <input
+              className="Input"
+              type="number"
+              value={inputValue}
+              onChange={(event) => setInputValue(event.target.value)}
+            />
+            <a
+              className="App-link"
+              target="_blank"
+              rel="noopener noreferrer"
+              onClick={tentativa}
+            >
+              Tentar
+            </a>
+          </>
+        )}
+        {screen === SCREENS.message && (
+          <>
+            <img src={number === parseInt(inputValue) ? genioSucesso : genioErro} className="App-logo" alt="logo" />
+            <p>{currentMessage}</p>
+            <a
+              className="App-link"
+              target="_blank"
+              rel="noopener noreferrer"
+              onClick={goBack}
+            >
+              Voltar
+            </a>
+          </>
+        )}
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
